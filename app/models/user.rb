100615e2d798759cbf5f4b6483abb724a0382882
Lawrence O <lawrence@studioimagine.me>
2016-03-15 04:40:15 +0000

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+  
+  # Follow gem
+  acts_as_followable
+  acts_as_follower
+  
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :token_authenticatable
+         
+  has_many :authentication_tokens
+  
+end
